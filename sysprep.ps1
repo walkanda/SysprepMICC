4c919f7cce16723119a96098468505588e504609
walkanda <85309747+walkanda@users.noreply.github.com>
2021-12-14 15:26:48 -0800

    Update sysprep.ps1

--- sysprep.ps1
+++ sysprep.ps1
@@ -1,5 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 set-executionpolicy unrestricted
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Import-Module ServerManager
 Import-Csv Roles.csv | foreach{Add-WindowsFeature $_.name  }
