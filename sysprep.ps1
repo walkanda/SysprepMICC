96ca81403d96e4c45b59d808f0027342330a838a
walkanda <85309747+walkanda@users.noreply.github.com>
2021-12-14 15:03:26 -0800

    Update sysprep.ps1

--- sysprep.ps1
+++ sysprep.ps1
@@ -1,2 +1,6 @@
 set-executionpolicy unrestricted
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Import-Module ServerManager
 Import-Csv Roles.csv | foreach{Add-WindowsFeature $_.name  }
+Install-Module PSWindowsUpdate
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
